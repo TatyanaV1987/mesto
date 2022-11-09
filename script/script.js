da1d6855d5f9f42e7db5154a98ac468fac12b1c0
Vadim Balashov <vadim@zebraworkshop.ru>
2022-11-09 18:20:23 +0300

    коммит

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
 const popup = document.querySelector('.popup');
 //найдем попапы по отдельности
-const popupEdit = document.querySelector('.popup__type_edit');
-const popupAdd = document.querySelector('.popup__type_add');
-const popupImage = document.querySelector('.popup__type_image');
+const popupEdit = document.querySelector('.popup_type_edit');
+const popupAdd = document.querySelector('.popup_type_add');
+const popupImage = document.querySelector('.popup_type_image');
 //найдем кнопки закрытия попап по отдельности
 const closePopupEdit = popupEdit.querySelector('.popup__close');
 const closePopupAdd = popupAdd.querySelector('.popup__close');
